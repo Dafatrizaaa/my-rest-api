854f047c9e55b969c2407dbacd9554a6ba39545b
Dz <99237626+Dafatrizaaa@users.noreply.github.com>
2024-04-01 14:24:08 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'Zeeoneofc'// yourname
 global.MONGO_DB_URI = "mongodb+srv://Dftrz:Arkanantakamban0@cluster0.dsc0r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "finky0822@gmail.com" //email
-global.email_password = "rpvi hqcz ssik kezz" //application password email
+global.your_email = "noreplyapidz@gmail.com" //email
+global.email_password = "dyyx jxcm upci rlkm" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
